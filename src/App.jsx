ec6cadea207029d5f6cddbeafa72d2a60a13939c
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-05-27 18:13:28 +0100

    testing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ function App() {
   return (
     <>
       <div className="bg-lime-500 flex justify-center p-4">
-        <h1 className="text-8xl ">Tailwind Test</h1>
-        <h3>Push test</h3>
+        <div>
+          <h1 className="text-8xl">Tailwind Test</h1>
+        </div>
       </div>
+
+      <section className="mt-4">
+        <div className="grid md:grid-cols-2 gap-4">
+          <div className="bg-slate-500 p-4 rounded-xl">
+            <h3 className="text-4xl">Testing Testing</h3>
+          </div>
+          <div className="bg-slate-500 p-4 rounded-xl">
+            <h3 className="text-4xl">Testing Testing</h3>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
