80771bc4e56eadfbb0c318d4ad15413949d2e47b
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-05-27 18:01:12 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-lime-500 flex justify-center p-4">
+        <h1 className="text-8xl ">Tailwind Test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
