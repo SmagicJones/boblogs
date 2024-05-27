e82e52442e84ce2b8e3feddc7f9f3960253d450f
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-05-27 18:04:22 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <>
       <div className="bg-lime-500 flex justify-center p-4">
         <h1 className="text-8xl ">Tailwind Test</h1>
+        <h3>Push test</h3>
       </div>
     </>
   );
